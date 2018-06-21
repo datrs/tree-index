e672aea2627681d1d6e9cf0c340a932de7dfd89b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-06-21 13:54:28 +0200

    upgrade Proof

--- src/proof.rs
+++ src/proof.rs
@@ -2,17 +2,53 @@
 ///
 /// Merkle trees are proven by checking the parent hashes.
 #[derive(Debug, PartialEq)]
-pub struct Proof {
-  /// Index of the node this was verified by.
-  pub verified_by: usize,
-
-  /// Merkle proof for the index you pass, written in `flat-tree` notation.
-  pub nodes: Vec<usize>,
+pub struct Proof<'a> {
+  verified_by: usize,
+  nodes: &'a [usize],
 }
 
-impl Proof {
-  /// Create a new [`Proof`] instance.
-  pub fn new(verified_by: usize, nodes: Vec<usize>) -> Self {
-    Proof { nodes, verified_by }
+impl <'a>Proof<'a> {
+  /// Create a new instance.
+  ///
+  /// ## Examples
+  /// ```rust
+  /// # extern crate tree_index;
+  /// # use tree_index::Proof;
+  /// let nodes = vec![];
+  /// let _proof = Proof::new(0, &nodes);
+  /// ```
+  #[inline]
+  pub fn new(verified_by: usize, nodes: &'a [usize]) -> Self {
+    Self { nodes, verified_by }
+  }
+
+  /// Get the index for the node which verifies the input index.
+  ///
+  /// ## Examples
+  /// ```rust
+  /// # extern crate tree_index;
+  /// # use tree_index::Proof;
+  /// let nodes = vec![];
+  /// let proof = Proof::new(0, &nodes);
+  /// assert_eq!(proof.verified_by(), 0);
+  /// ```
+  #[inline]
+  pub fn verified_by(&self) -> usize {
+    self.verified_by
+  }
+
+  /// Merkle proof for the index you pass, written in `flat-tree` notation.
+  ///
+  /// ## Examples
+  /// ```rust
+  /// # extern crate tree_index;
+  /// # use tree_index::Proof;
+  /// let nodes = vec![];
+  /// let proof = Proof::new(0, &nodes);
+  /// assert_eq!(proof.nodes.to_owned(), vec![]);
+  /// ```
+  #[inline]
+  pub fn nodes(&self) -> &[usize] {
+    &self.nodes
   }
 }
